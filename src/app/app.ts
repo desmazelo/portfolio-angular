793509f9f9468a7885d33c8e714a7441c9467759
Rafael Campos <only_roc@hotmail.com>
2025-11-26 22:00:51 -0300

    chore: commit inicial do portfólio em Angular + Tailwind

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+// import { bootstrapApplication } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,About, Skills,Projects,Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('portfolio-angular');
 }
+// bootstrapApplication(App);
